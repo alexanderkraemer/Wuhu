1e6a29984dba024015492dfbe0c5b4b743abe436
Alexander Krämer <office@alexanderkraemer.com>
2016-11-25 16:09:46 +0100

    update

--- GenerateTestData/Program.cs
+++ GenerateTestData/Program.cs
@@ -11,20 +11,45 @@ namespace WuHu.GenerateTestData
 {
     class Program
     {
-        private IDatabase database;
+        private static IDatabase database;
 
         static void Main(string[] args)
         {
+            database = DalFactory.CreateDatabase();
+
         }
 
         private void GenerateRoles()
         {
-            database = DalFactory.CreateDatabase();
 
             RoleDao dao = new RoleDao(database); 
 
-            Role role = new Role("Admin");
+            Role admin = new Role("Admin");
+            Role member = new Role("Member");
+            Role editor = new Role("Editor");
 
+            dao.Insert(admin);
+            dao.Insert(member);
+            dao.Insert(editor);
+        }
+
+        private void GenerateDays()
+        {
+            DayDao dao = new DayDao(database);
+
+            Day montag = new Day("Montag");
+            Day dienstag = new Day("Dienstag");
+            Day mittwoch = new Day("Mittwoch");
+            Day donnerstag = new Day("Donnerstag");
+            Day freitag = new Day("Freitag");
+            Day samstag = new Day("Samstag");
+
+            dao.Insert(montag);
+            dao.Insert(dienstag);
+            dao.Insert(mittwoch);
+            dao.Insert(donnerstag);
+            dao.Insert(freitag);
+            dao.Insert(samstag);
         }
     }
 }

dc0c39949b2aebfddfbb500a085557f6a258880d
Alexander Krämer <office@alexanderkraemer.com>
2016-11-25 16:00:48 +0100

    forgot to save project

--- GenerateTestData/Program.cs
+++ GenerateTestData/Program.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WuHu.Common;
 using WuHu.Domain;
-
+using WuHu.SQLServer;
 
 namespace WuHu.GenerateTestData
 {
     class Program
     {
+        private IDatabase database;
+
         static void Main(string[] args)
         {
         }
 
         private void GenerateRoles()
         {
-            Role 
+            database = DalFactory.CreateDatabase();
+
+            RoleDao dao = new RoleDao(database); 
+
+            Role role = new Role("Admin");
+
         }
     }
 }

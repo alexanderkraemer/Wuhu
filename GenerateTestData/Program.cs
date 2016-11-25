a990409f4f2d6d55cf19057d41d005e2f291a6b9
Alexander Krämer <office@alexanderkraemer.com>
2016-11-25 15:59:35 +0100

    generatetestdata project added

---
+++ GenerateTestData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WuHu.Domain;
+
+
+namespace WuHu.GenerateTestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private void GenerateRoles()
+        {
+            Role 
+        }
+    }
+}
